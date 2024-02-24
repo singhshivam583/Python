a8427732390d3aa06b7a3e94dfc96f9b59eaddc8
Shvam Singh <singhshivam583@yahoo.in>
2024-02-24 16:13:21 +0530

    decorators

--- 07_oops/02_sol.py
+++ 07_oops/02_sol.py
@@ -6,12 +6,11 @@ class Car:
     def fullName(self):
         return f"{self.brand} {self.model}"
 
-# Inheritance
-class Electric_Car(Car):   # Inheritance - electric car inherits from car class
-    def __init__(self,brand, model, battery_size):
-        super().__init__(brand, model)     # calling parent constructor using super() method
-        self.battery_size = battery_size
+my_Car = Car("Ford", "Mustang")
+print(my_Car.brand) 
+print(my_Car.model)
 
-my_Tesla = Electric_Car("Tesla", "Model S", "75KWh")
-print(f"The car is an {my_Tesla.fullName()} and has a {my_Tesla.battery_size} capacity battery.")
+myNew_Car = Car("Toyota", "Corolla")
+print(myNew_Car.fullName()) 
+
 
